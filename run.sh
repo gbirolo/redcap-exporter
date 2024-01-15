80040e0a79fd1f4046af9078b77c4f4081e22253
Giovanni Birolo <giovanni.birolo@unito.it>
2024-01-15 11:04:07 +0000

    added launch instructions

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. .venv/bin/activate
+#flask --app redcap_exporter run
+#https://compbiomed.hpc4ai.unito.it/redcap-exporter/
+gunicorn --workers 4 --bind '127.0.0.1:10060' 'redcap_exporter:create_app()'
